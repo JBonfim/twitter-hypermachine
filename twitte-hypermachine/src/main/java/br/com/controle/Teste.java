8f4288f2db80417a6c3d66007e33fa93fbf09381
Jabez <Jabez@Jabes>
2014-02-16 01:34:43 -0300

    nova atualizacao

--- twitte-hypermachine/src/main/java/br/com/controle/Teste.java
+++ twitte-hypermachine/src/main/java/br/com/controle/Teste.java
@@ -1,7 +1,13 @@
 package br.com.controle;
 
 public class Teste {
-
+	private int num;
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public int getNum() {
+		return num;
+	}
 	/**
 	 * @param args
 	 */
