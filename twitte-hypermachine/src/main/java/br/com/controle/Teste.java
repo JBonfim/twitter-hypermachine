1c4a9d700e314ee5985f1d7c3aa74e146644b1dc
Jabez <Jabez@Jabes>
2014-02-15 23:28:26 -0300

    classe teste

---
+++ twitte-hypermachine/src/main/java/br/com/controle/Teste.java
@@ -0,0 +1,14 @@
+package br.com.controle;
+
+public class Teste {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HHHHH");
+
+	}
+
+}
