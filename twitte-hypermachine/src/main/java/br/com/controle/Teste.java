7d0fb97aa3c9cd9d2b786326a34876e49d96bbec
JBonfim <jabes.bonfim@dce.ufpb.br>
2014-02-16 01:08:43 -0200

    Update Teste.java
    Nova atualização

--- twitte-hypermachine/src/main/java/br/com/controle/Teste.java
+++ twitte-hypermachine/src/main/java/br/com/controle/Teste.java
@@ -8,6 +8,8 @@ public class Teste {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Atualizado pelo eclipse");
+                System.out.println("Atualizado pelo Git");
+
 	}
 
 }
