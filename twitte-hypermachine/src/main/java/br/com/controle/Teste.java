5a7841db664f3ee3fa4497be3afa94d2ca39abbf
Jabez <Jabez@Jabes>
2014-02-16 01:41:52 -0300

    novo

--- twitte-hypermachine/src/main/java/br/com/controle/Teste.java
+++ twitte-hypermachine/src/main/java/br/com/controle/Teste.java
@@ -8,13 +8,13 @@ public class Teste {
 	public int getNum() {
 		return num;
 	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Atualizado pelo eclipse");
-                System.out.println("Atualizado pelo Git");
+		
 
 	}
 
